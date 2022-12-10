3a4c7ef79f5d3929c20bc1ff3253dc99ae81b896
john <john@real-currents.com>
2022-12-10 09:28:32 -0500

    deploy amplify app

--- lib/cdk-amplify-backend-stack.ts
+++ lib/cdk-amplify-backend-stack.ts
@@ -1,18 +1,17 @@
-import * as cdk from '@aws-cdk/core';
-import { Construct, SecretValue, Stack, StackProps } from "@aws-cdk/core";
+import * as cdk from "@aws-cdk/core";
 import * as amplify from "@aws-cdk/aws-amplify";
 import * as cognito from "@aws-cdk/aws-cognito";
 import * as iam from "@aws-cdk/aws-iam";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class BackendTsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CdkAmplifyBackendStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // The code that defines your cdk.Stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'BackendTsQueue', {
+    // const queue = new sqs.Queue(this, 'CdkAmplifyBackendQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
@@ -31,40 +30,30 @@ export class BackendTsStack extends Stack {
 
     // ... above ^ is commented out, because I will re-use existing userPool and userPoolClient
 
-    const identityPool = new cognito.CfnIdentityPool(this, "AmplifyCDKIdentityPool", {
-      allowUnauthenticatedIdentities: true,
-      cognitoIdentityProviders: [{
-        clientId: `${SecretValue.secretsManager("userPoolClientId").unsafeUnwrap()}`,  //userPoolClient.userPoolClientId,
-        providerName: `cognito-idp.us-east-1.amazonaws.com/${SecretValue.secretsManager("userPoolId").unsafeUnwrap()}`      // userPool.userPoolProviderName
-      }]
-    });
+    // const identityPool = new cognito.CfnIdentityPool(this, "AmplifyCDKIdentityPool", {
+    //   allowUnauthenticatedIdentities: true,
+    //   cognitoIdentityProviders: [{
+    //     clientId: `${cdk.SecretValue.secretsManager("userPoolClientId").unsafeUnwrap()}`,  //userPoolClient.userPoolClientId,
+    //     providerName: `cognito-idp.us-east-1.amazonaws.com/${cdk.SecretValue.secretsManager("userPoolId").unsafeUnwrap()}`      // userPool.userPoolProviderName
+    //   }]
+    // });
 
-    const serviceRole = iam.Role.fromRoleArn(
-        this,
-        'amplify-console',
-        `${SecretValue.secretsManager("amplifyServiceRole").unsafeUnwrap()}`,
-        // aws secretsmanager create-secret --name amplifyServiceRole  --secret-string '***'
-        // {
-        //   "ARN": "arn:aws:secretsmanager:us-east-1:312512371189:secret:amplifyServiceRole-LfBaw3",
-        //     "Name": "amplifyServiceRole",
-        //     "VersionId": "0816f534-0f32-46cd-9bff-e4eeeed5bc5a"
-        // }
-    { mutable: false },
-    );
+    // const serviceRole = iam.Role.fromRoleArn(
+    //     this,
+    //     'amplify-console',
+    //     `${cdk.SecretValue.secretsManager("amplifyServiceRole").unsafeUnwrap()}`,
+    //     // aws secretsmanager create-secret --name amplifyServiceRole  --secret-string '***'
+    //     { mutable: false },
+    // );
 
     // AWS Amplify App: Frontend
-    const app = new amplify.App(this, "amplify-frontend", {
-      role: serviceRole,
+    const amplifyApp = new amplify.App((<cdk.Construct>(this)), "amplify-frontend", {
+      // role: serviceRole,
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
         owner: "Real-Currents",
         repository: "cdk-amplify-frontend",
-        oauthToken: SecretValue.secretsManager("gh_token")
+        oauthToken: cdk.SecretValue.secretsManager("gh_token")
         // $ aws secretsmanager create-secret --name gh_token --secret-string $GITHUB_PAT
-        // {
-        //   "ARN": "arn:aws:secretsmanager:us-east-1:312512371189:secret:gh_token-GkY8AT",
-        //   "Name": "gh_token",
-        //   "VersionId": "198ab2fe-4895-461c-be21-e88865ac836e"
-        // }
       }),
       autoBranchCreation: { // Automatically connect branches that match a pattern set
         patterns: [ 'main*', 'dev*' ],
@@ -73,13 +62,13 @@ export class BackendTsStack extends Stack {
         'APP_VERSION': "1",
         'API_BACKEND': "https://frontend.ruralopportunitymap.us",
         'VERSION_AMPLIFY': "10.5.2",
-        'USER_POOL_ID': `${SecretValue.secretsManager("userPoolId").unsafeUnwrap()}`,                // userPool.userPoolId,
-        'USER_POOL_CLIENT_ID': `${SecretValue.secretsManager("userPoolClientId").unsafeUnwrap()}`,  // userPoolClient.userPoolClientId,
-        'IDENTITY_POOL_ID': identityPool.ref,
-        'REGION': this.region
+        // 'USER_POOL_ID': `${cdk.SecretValue.secretsManager("userPoolId").unsafeUnwrap()}`,                // userPool.userPoolId,
+        // 'USER_POOL_CLIENT_ID': `${cdk.SecretValue.secretsManager("userPoolClientId").unsafeUnwrap()}`,  // userPoolClient.userPoolClientId,
+        // 'IDENTITY_POOL_ID': identityPool.ref,
+        // 'REGION': this.region
       }
     });
 
-    const frontendMainBranch = app.addBranch("main")
+    amplifyApp.addBranch("main")
   }
 }
